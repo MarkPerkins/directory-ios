bcbe66fb42ba052cb4d6adba5d2c5b47bd72bbfe
Mark Perkins <xperkinsx@gmail.com>
2022-06-18 09:43:34 -0400

    setup basic table view

--- Directory/Directory/DirectoryViewController.swift
+++ Directory/Directory/DirectoryViewController.swift
@@ -7,13 +7,83 @@ class DirectoryViewController: UIViewController {
     init(viewModel: DirectoryViewModel = DirectoryViewModel()) {
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
+        title = "Employee Directory"
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: CGRect.zero, style: .grouped)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.separatorStyle = .none
+        tableView.register(EmployeeTableViewCell.self, forCellReuseIdentifier: EmployeeTableViewCell.defaultReuseIdentifier)
+        return tableView
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupViews()
+        setupConstraints()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        tableView.reloadData()
+    }
+
+    private func setupViews() {
+        view.addSubview(tableView)
+    }
+
+    private func setupConstraints() {
+        var constraints = [NSLayoutConstraint]()
+        constraints.append(tableView.topAnchor.constraint(equalTo: view.topAnchor))
+        constraints.append(tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor))
+        constraints.append(tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor))
+        constraints.append(tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor))
+        NSLayoutConstraint.activate(constraints)
+    }
+}
+
+extension DirectoryViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        50
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: EmployeeTableViewCell.defaultReuseIdentifier, for: indexPath) as! EmployeeTableViewCell
+        cell.textLabel?.text = "This is a test"
+        return cell
+    }
+}
+
+extension DirectoryViewController: UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 110
+    }
+
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 0
+    }
+
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 0
+    }
+
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        return UIView(frame: .zero)
+    }
+
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        return UIView(frame: .zero)
     }
 }
--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 ## Build tools & versions used
 - Swift, XCode 13.4, Reworked a SwiftUI template to launch via Scene/App Delegate.
+- Revealapp.com for assitance setting up layout
 
 ## Steps to run the app
 
@@ -10,10 +11,13 @@
 ## How long did you spend on this project?
 
 ## Did you make any trade-offs for this project? What would you have done differently with more time?
+- I built this using UITableView, but would have spent more time implementing this in a CollectionView as i feel it can provide greater flexibility as your featureset enhances.  For the sake of simplicity i made the trade off to create a UITableView instead.
 
 ## What do you think is the weakest part of your project?
 
 ## Did you copy any code or dependencies? Please make sure to attribute them here!
+As a personal challenge, i tried to just put this together without any.  That said, a couple caveats.
+- I did pull in a few extensions from code i've written in the past to simplify things.
 
 ## Is there any other information you’d like us to know?
 - Unit tests are places next to their respective owner/class within the project hierarchy.
