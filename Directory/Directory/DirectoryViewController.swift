36664cdaa144000013ba4f5fbaa1bb7dbb741d58
Mark Perkins <xperkinsx@gmail.com>
2022-06-18 09:05:25 -0400

    setup initial view

---
+++ Directory/Directory/DirectoryViewController.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+class DirectoryViewController: UIViewController {
+
+    let viewModel: DirectoryViewModel
+
+    init(viewModel: DirectoryViewModel = DirectoryViewModel()) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- Directory/SceneDelegate.swift
+++ Directory/SceneDelegate.swift
@@ -8,10 +8,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = scene as? UIWindowScene else { return }
 
-        let rootView = RootView()
+        let rootViewController = DirectoryViewController()
 
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = UIHostingController(rootView: rootView)
+        window.rootViewController = rootViewController
         self.window = window
         window.makeKeyAndVisible()
     }
