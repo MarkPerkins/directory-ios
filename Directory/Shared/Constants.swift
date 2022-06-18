8ceacf68798fa8a4adac1904333f03e9425ec556
Mark Perkins <xperkinsx@gmail.com>
2022-06-18 08:57:27 -0400

    reorder project files.
    - add constants for networking

---
+++ Directory/Shared/Constants.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+class Constants {
+
+    struct Network {
+        static let employeeEndpoint = "https://s3.amazonaws.com/sq-mobile-interview/employees.json"
+    }
+
+    struct MockResponses {
+        static let malformed = "https://s3.amazonaws.com/sq-mobile-interview/employees_malformed.json"
+        static let empty = "https://s3.amazonaws.com/sq-mobile-interview/employees_empty.json"
+    }
+}
