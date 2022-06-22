518ba7a02170aa62cbfd9fe8c316a6c2ebfd52ff
Mark Perkins <xperkinsx@gmail.com>
2022-06-22 10:01:35 -0400

    add a unit test for viewmodel and setup mock data

---
+++ Directory/Directory/DirectoryViewModelTests.swift
@@ -0,0 +1,32 @@
+@testable import Directory
+import XCTest
+
+class DirectoryViewModelTests: XCTestCase {
+
+    func testValidResponseStoresEmployees() {
+        let successService = SuccessfulMockDirectoryService()
+        let viewModel = DirectoryViewModel(directoryService: successService)
+        let expectation = expectation(description: "loademployees_completed")
+        viewModel.loadEmployees { success in
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 1.0)
+        XCTAssertNotNil(viewModel.employees)
+    }
+}
+
+class SuccessfulMockDirectoryService: DirectoryAPI {
+
+    func fetchEmployees(sortedBy: DirectorySort, completion: @escaping (Result<[Employee], DirectoryServiceError>) -> Void) {
+        let json = loadJsonFile("success")
+        let result = try! JSONDecoder().decode(Directory.self, from: json!)
+        completion(.success(result.employees))
+    }
+
+    private func loadJsonFile(_ fileName: String) -> Data? {
+        if let path = Bundle.main.path(forResource: fileName, ofType: "json") {
+            return try! Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
+        }
+        return nil
+    }
+}
